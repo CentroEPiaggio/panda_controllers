b3887a87652d57a73c98750fd11cc491b62e52ba
Maverik800 <d.nanni@hotmail.it>
2020-01-21 15:23:31 +0100

    Update the h library for the pdcontroller.

--- include/panda_controllers/pdcontroller.h
+++ include/panda_controllers/pdcontroller.h
@@ -13,9 +13,11 @@
 #include <ros/node_handle.h>
 #include <ros/time.h>
 
-#include <controller_interface/multi_interface_controller.h>
+#include <franka/robot_state.h> //so we can call the qdesired
 
-#include <sensor_msgs/JointState.h>
+#include <controller_interface/multi_interface_controller.h> //to use multiple template in the class definition
+
+#include <sensor_msgs/JointState.h>//for the callback, it can read the topic
 
 //ROS message
 #include <franka_msgs/FrankaState.h>
@@ -37,16 +39,17 @@ hardware_interface::EffortJointInterface, franka_hw::FrankaStateInterface> {
   //topic's command listened 
   ros::Subscriber sub_command_;
   
-  //variables
-  double error, old_error,dot_error,commanded_effort,dt;
-   
-  //Command setting Callback
-  void setCommandCB(const sensor_msgs::JointStateConstPtr &msg);
-  sensor_msgs::JointState command_; //definition of the desiderd position 
+  //variables used in updating
+  double error, old_error,dot_error,commanded_effort,dt; //definition of the errors and the variable for the commanded effort 
   double Kp, Kv; //let's define the position and velocity gains
-  hardware_interface::JointHandle joint_; //Selection of the joints
   
-  ros::Duration elapsed_time_; //?
+  //Command setting Callback
+  void setCommandCB(const sensor_msgs::JointStateConstPtr& msg);
+  sensor_msgs::JointState command_; //definition of the desiderd position 
+  
+  std::unique_ptr<franka_hw::FrankaStateHandle> state_handle_; //used for the state of the joints and command the torque
+  
+ 
 };
 
 }  // namespace panda_controllers
