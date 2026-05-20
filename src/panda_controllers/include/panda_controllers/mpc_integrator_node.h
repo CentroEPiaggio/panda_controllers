34a914d15f6362fbd2edf3a9aad8b183bf5df666
eligiomansi <eligiomansi14@gmail.com>
2026-05-20 11:49:45 +0200

    fix_mpc_integrator_node.h

--- src/panda_controllers/include/panda_controllers/mpc_integrator_node.h
+++ src/panda_controllers/include/panda_controllers/mpc_integrator_node.h
@@ -3,9 +3,12 @@
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
 #include <Eigen/Dense>
+#include <geometry_msgs/TwistStamped.h>
 
-// Msg MPC
+// Msg personalizzati
 #include <panda_controllers/MpcSolution.h>
+#include <panda_controllers/ObstacleStatus.h>
+#include <gazebo_msgs/ModelStates.h>
 
 namespace panda_controllers
 {
@@ -21,7 +24,8 @@ private:
     // =========================
     void mpcCallback(const panda_controllers::MpcSolutionConstPtr &msg);
     void jointStatesCallback(const sensor_msgs::JointStateConstPtr &msg);
-    void controlLoop(const ros::TimerEvent &);
+    void pallaCallback(const gazebo_msgs::ModelStates::ConstPtr &msg);
+    void controlLoop(const ros::TimerEvent &event);
 
     void publishFilteredStateNow();
 
@@ -30,11 +34,15 @@ private:
     // =========================
     ros::NodeHandle nh;
 
+    // Subscriber
     ros::Subscriber sub_mpc_sol;
     ros::Subscriber sub_joint_states;
+    ros::Subscriber sub_palla;
 
+    // Publisher
     ros::Publisher pub_cmd;
     ros::Publisher pub_filtered_state;
+    ros::Publisher pub_palla_filt;
 
     ros::Timer timer;
 
@@ -63,6 +71,7 @@ private:
     Eigen::VectorXd ddq_filt_;
 
     Eigen::VectorXd dq_prev_;
+    ros::Time last_joint_time_;
     bool first_joint_msg_ = true;
 
     // =========================
@@ -71,6 +80,34 @@ private:
     double alpha_q_ = 0.5;
     double alpha_dq_ = 0.2;
     double alpha_ddq_ = 0.05;
+
+    // =========================
+    // Stato palla (obstacle)
+    // =========================
+    Eigen::Vector3d p_palla_filt_;
+    Eigen::Vector3d v_palla_filt_;
+    Eigen::Vector3d p_palla_prev_;
+    ros::Time last_palla_time_;
+    bool first_palla_msg_ = true;
+
+    double alpha_p_palla_ = 0.7;
+    double alpha_v_palla_ = 0.1;
+
+    // =========================
+    // Limiti fisici Franka
+    // =========================
+    static constexpr double q_min[7] = {-2.8973, -1.7628, -2.8973, -3.0718, -2.8973, -0.0175, -2.8973};
+    static constexpr double q_max[7] = {2.8973, 1.7628, 2.8973, -0.0698, 2.8973, 3.7525, 2.8973};
+    static constexpr double dq_min[7] = {-2.175, -2.175, -2.175, -2.175, -2.61, -2.61, -2.61};
+    static constexpr double dq_max[7] = {2.175, 2.175, 2.175, 2.175, 2.61, 2.61, 2.61};
+    static constexpr double ddq_min[7] = {-15, -7.5, -10, -12.5, -15, -20, -20};
+    static constexpr double ddq_max[7] = {15, 7.5, 10, 12.5, 15, 20, 20};
+
+    // =========================
+    // Controllo temporale
+    // =========================
+    ros::Time last_control_time_;
+    bool first_control_ = true;
 };
 
 } // namespace panda_controllers
